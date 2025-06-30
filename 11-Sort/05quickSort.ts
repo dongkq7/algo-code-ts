ad9ea7eaa3ae22a1b29e3d80c4fd95b566eaedf6
dongkaiqi <87631658@qq.com>
2025-06-30 16:55:40 +0800

    feat: 更新快速排序

--- 11-Sort/05quickSort.ts
+++ 11-Sort/05quickSort.ts
@@ -1,43 +1,62 @@
-import { swap, testSort } from "./utils"
-
-function quickSort(arr: number[]): number[] {
-  // 首先对整个数组区域数据进行以基准元素为标准的排序
-  partition(0, arr.length - 1)
-
-  function partition(left: number, right: number) {
-    // 注意此时需要加上结束递归调取的条件
-    if (left >= right) return
-    const qivot = arr[right]
-    // 设置两个指针分别指向开始位置与基准元素的前一个位置
-    let i = 0
-    let j = right - 1
-  
-    // 直到i大于j时结束
-    while(i <= j) {
-      // i向右移动直到找到比qivot大的元素
-      while(arr[i] < qivot) {
-        i++
-      }
-      // i向左移动直到找到比qivot小的元素
-      while(arr[j] > qivot) {
-        j--
-      }
-      // 如果此时i还比j小那么进行交换
-      if (i < j) {
-        swap(arr, i , j)
-        // 交换后i与j继续移动
-        i++
-        j--
-      }
-    }
-    // 结束后将基准元素与i位置的元素进行交换
-    swap(arr, i, right)
-    // 对基准元素左部分区域数据进行同样方式的排序
-    partition(left, j)
-    // 对基准元素右部分区域数据进行同样方式的排序
-    partition(i + 1, right)
+// 快速排序
+function quickSort(arr: number[]) {
+  if (arr.length < 2) {
+    return
   }
-  return arr
+  process(arr, 0, arr.length - 1)
 }
 
-testSort(quickSort)
+function process(arr: number[], left: number, right: number) {
+  if (left < right) {
+    // 随机找打一个数，最为基准值，并放到数组的最后一个位置
+    const randomIndex = left + Math.floor(Math.random() * (right - left + 1))
+    swap(arr, randomIndex, right)
+
+    // 开始进行分治处理
+    const p = partition(arr, left, right)
+    // 递归处理左半部分
+    process(arr, left, p[0] - 1)
+    // 递归处理右半你分
+    process(arr, p[1] + 1, right)
+  }
+}
+
+function partition(arr: number[], left: number, right: number) {
+  // less区域用于存放小于基准值的数
+  // less区域的右边界，初始为数组的最左侧-1的位置
+  let less = left - 1
+  // more区域的右边界大于基准值的数
+  let more = right
+  // 取数组最后一个位置的数作为基准值
+  const pivot = arr[right]
+  // left指针没有到达more区域时进行循环遍历
+  while(left < more) {
+    if(arr[left] < pivot) {
+      // 小于基准值，则与less区域边界的下一个位置的数交换
+      // 并且less区域向后扩一位，left指向下一个数
+      swap(arr, ++less, left++)
+    } else if (arr[left] > pivot) {
+      // 比基准值大，那么与more区域左边界的前一个数进行交换
+      // 并且more区域向左扩一位，left保持不变
+      swap(arr, --more, left)
+    } else {
+      // 如果与基准值相等，那么直接向后移动
+      left++
+    }
+  }
+  // 来到这里，left与more的值相同
+  // 接下来就需要交换right位置的基准值与more位置的数
+  swap(arr, more, right)
+  // 返回边界
+  return [less + 1, more]
+}
+
+function swap(arr: number[], i: number, j: number) {
+  let temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+
+const arr = [2,8,7,3,0,5,9,5]
+quickSort(arr)
+console.log(arr)
--- 11-Sort/utils.ts
+++ 11-Sort/utils.ts
@@ -1,18 +1,18 @@
 type SortFunType = (arr: number[]) => number[]
 
 
-// export function swap(arr: number[], i: number, j: number) {
-//   const temp = arr[i]
-//   arr[i] = arr[j]
-//   arr[j] = temp
-//    // [arr[i], arr[j]] = [arr[j], arr[i]]
-// }
-
 export function swap(arr: number[], i: number, j: number) {
-  arr[i] = arr[i] ^ arr[j]
-  arr[j] = arr[i] ^ arr[j]
-  arr[i] = arr[i] ^ arr[j]
+  const temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+   // [arr[i], arr[j]] = [arr[j], arr[i]]
 }
+
+// export function swap(arr: number[], i: number, j: number) {
+//   arr[i] = arr[i] ^ arr[j]
+//   arr[j] = arr[i] ^ arr[j]
+//   arr[i] = arr[i] ^ arr[j]
+// }
 
 export function testSort(sortFun: SortFunType) {
   const arr = Array.from({length: 10}, () => {
